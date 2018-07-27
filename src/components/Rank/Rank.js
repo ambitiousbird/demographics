15bca9e8e1c3e74fd106ba179ab010bcc990248c
Lang Gao <langfr.gao@gmail.com>
2018-07-27 00:54:59 -0400

    first commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rank=({name,entries})=>{
+    return (
+        <div>
+            <div className='f3 white'>
+                {`${name}, your analysis count is...`}
+            </div>
+            <div className='f1 white'>
+                {entries}
+            </div>
+    </div>
+    );
+}
+export default Rank;
